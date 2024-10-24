f051f468126a14f42a3ce7b0beb04eec81cc2454
unknown <juho.kettunen@tuni.fi>
2024-10-24 21:20:13 +0300

    uuuhhh

--- test/banana.test.js
+++ test/banana.test.js
@@ -2,7 +2,7 @@ import chai from 'chai';
 var except = chai.expect;
 import banana from '../banana.js';
 
-describe('test', () => {
+describe('testaaaa', () => {
     it('except to add', () => {
         except(banana(1,1)).to.equal(1);
     })
--- test/stuff.test.js
+++ test/stuff.test.js
@@ -3,14 +3,3 @@ var except = chai.expect;
 import f1 from '../stuff.js';
 import f2 from '../stuff.js';
 
-describe('test', () => {
-    it('f1 test', () => {
-        except(f1(2,1)).to.equal(1);
-    })
-})
-
-describe('test2', () => {
-    it('f2 test', () => {
-        except(f1(2,1)).to.equal(1);
-    })
-})

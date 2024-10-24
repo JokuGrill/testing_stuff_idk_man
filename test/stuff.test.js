006b23c5f9537682a1207e0246648f652f50bd99
unknown <juho.kettunen@tuni.fi>
2024-10-24 21:10:39 +0300

    it fucking works lets gooo

--- test/stuff.test.js
+++ test/stuff.test.js
@@ -1,9 +1,16 @@
 import chai from 'chai';
 var except = chai.expect;
 import f1 from '../stuff.js';
+import f2 from '../stuff.js';
 
 describe('test', () => {
     it('f1 test', () => {
+        except(f1(2,1)).to.equal(1);
+    })
+})
+
+describe('test2', () => {
+    it('f2 test', () => {
         except(f1(2,1)).to.equal(1);
     })
 })
